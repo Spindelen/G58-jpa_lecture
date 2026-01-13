25ce5f97d9588f6be6d83ab4471d1cc7f9a162a0
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2026-01-13 09:41:39 +0100

    Initialize Spring Boot project with Maven wrapper and basic configuration.

---
+++ src/main/java/se/lexicon/g58jpa_lecture/G58JpaLectureApplication.java
@@ -0,0 +1,14 @@
+package se.lexicon.g58jpa_lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G58JpaLectureApplication {
+
+    public static void main(String[] args) {
+        // Do not write more code here...
+        SpringApplication.run(G58JpaLectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/g58jpa_lecture/G58JpaLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.g58jpa_lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G58JpaLectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
