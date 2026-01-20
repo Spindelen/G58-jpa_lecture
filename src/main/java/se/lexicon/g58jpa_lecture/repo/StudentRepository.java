d4223e50efaa70dfb9b5f75c39d342705802263b
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2026-01-20 10:10:51 +0100

    Revert the Stacktrace Experiment in `StudentRepository`

--- src/main/java/se/lexicon/g58jpa_lecture/repo/StudentRepository.java
+++ src/main/java/se/lexicon/g58jpa_lecture/repo/StudentRepository.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, String> {
 
-    List<Student> findByfirstname(String firstName);
+    List<Student> findByFirstName(String firstName);
 
     Optional<Student> findByEmail(String email);
 
